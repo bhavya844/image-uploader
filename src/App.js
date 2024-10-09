cb55c9ee12808fa8542eb24b4ba27c0e7b90f27b
Bhavya Mukesh Dave <bh392017@dal.ca>
2024-10-09 17:35:22 -0300

    modified: add more routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { ToastContainer } from 'react-toastify';
 import Register from './components/Register';
 import Login from './components/Login';
 import LandingPage from './components/LandingPage';
+import UploadPhoto from './components/UploadPhoto';
+import ViewAllPhotos from './components/ViewAllPhotos';
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
           <Route path="/" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/landing" element={<LandingPage />} />
+          <Route path="/upload-photo" element={<UploadPhoto />} />
+          <Route path="/view-all-photos" element={<ViewAllPhotos />} />
         </Routes>
       </div>
     </Router>
